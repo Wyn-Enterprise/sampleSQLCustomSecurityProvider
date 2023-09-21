501014fcb0f92b7c8acad9d3ceb8b1d00a21d422
Bhupesh Malhotra <bhupeshmalhotra@gmail.com>
2023-09-21 15:29:51 -0400

    upgraded to Miscrosft.Data.SqlClient and .NET6

--- SqlDBSecurityProvider.cs
+++ SqlDBSecurityProvider.cs
@@ -4,7 +4,7 @@ using GrapeCity.Enterprise.Identity.SecurityProvider;
 using System;
 using System.Collections.Generic;
 using System.Data.Common;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
